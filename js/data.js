c6519663acfccc3bbf34260a0d36b7378c5548da
libra guest <julien.gotteland@laposte.net>
2012-10-25 11:12:01 -0400

    Various improvements

--- js/data.js
+++ js/data.js
@@ -5,18 +5,18 @@ $(function () {
         minData = [],
         avgData = [],
         maxData = [],
-        success = [],
-        running = [],
-        failure = [],
+        successData = [],
+        runningData = [],
+        failureData = [],
         nbDays = 0;
             
     for (var current = from; current <= to ; current = new Date(current + 24 * 3600 * 1000).getTime()) {
         minData[nbDays] = Math.floor(Math.random() * 1000 * 20);
         avgData[nbDays] = Math.floor(Math.random() * 1000 * 60 * 2) + 1000 * 20;
         maxData[nbDays] = Math.floor(Math.random() * 1000 * 60 * 8) + 1000 * 20 + 1000 * 60 * 2;
-        success[nbDays] = Math.floor(Math.random() * 1000) + 2000;
-        running[nbDays] = Math.floor(Math.random() * 50) + 50;
-        failure[nbDays] = Math.floor(Math.random() * 400) + 100;
+        successData[nbDays] = Math.floor(Math.random() * 1000) + 2000;
+        runningData[nbDays] = Math.floor(Math.random() * 50) + 50;
+        failureData[nbDays] = Math.floor(Math.random() * 400) + 100;
                 
         ++nbDays;
     }
@@ -293,7 +293,7 @@ $(function () {
     $('<div id="detail-chartContainer">').css({ height: 600 }).appendTo($chartContainer);
     $('<div id="master-chartContainer">').css({ position: 'absolute', top: 600, height: 100, width: '100%' }).appendTo($chartContainer);
     
-    createMaster();
+    //createMaster();
     
     var stockCHart;
     
@@ -309,8 +309,12 @@ $(function () {
             enabled: true,
             floating: true,
             reversed: true,
-            verticalAlign: 'top',
-            y: 10
+            verticalAlign: 'top'
+        },
+        plotOptions: {
+            area: {
+                stacking: 'percent'
+            }
         },
 		rangeSelector : {
 			selected : 0 // 1 month
@@ -321,35 +325,65 @@ $(function () {
                 data : maxData,
                 pointInterval: 24 * 3600 * 1000,
                 pointStart: from,
-                type: 'spline'
+                type: 'spline',
+                yAxis: 0
             }, {
                 name : 'Avg',
                 data : avgData,
                 pointInterval: 24 * 3600 * 1000,
                 pointStart: from,
-                type: 'spline'
+                type: 'spline',
+                yAxis: 0
             }, {
                 name : 'Min',
                 data : minData,
                 pointInterval: 24 * 3600 * 1000,
                 pointStart: from,
-                type: 'spline'
+                type: 'spline',
+                yAxis: 0
+            }, {
+                name : 'Failure',
+                data : failureData,
+                pointInterval: 24 * 3600 * 1000,
+                pointStart: from,
+                type: 'area',
+                yAxis: 1
+            }, {
+                name : 'Running',
+                data : runningData,
+                pointInterval: 24 * 3600 * 1000,
+                pointStart: from,
+                type: 'area',
+                yAxis: 1
+            }, {
+                name : 'Success',
+                data : successData,
+                pointInterval: 24 * 3600 * 1000,
+                pointStart: from,
+                type: 'area',
+                yAxis: 1
             }
         ],
-        subtitle: {
-            text: '(in hours and minutes)'
-        },
         title : {
-            text : 'Jobs Duration over Time'
+            text : 'Jobs Duration and Status over Time'
 		},
         tooltip: {
             crosshairs: true,
             formatter: function() {
-                var s = '<b>'+ Highcharts.dateFormat('%A %d %B %Y', this.x) +'</b><table><tr><td colspan="2">&nbsp;</td></tr>';
-
+                var s = '<b>'+ Highcharts.dateFormat('%A %d %B %Y', this.x + 24 * 3600 * 1000) +'</b><table><tr><td colspan="3">&nbsp;</td></tr>';
+                var duration = ['Min', 'Avg', 'Max'];
+                
                 $.each(this.points, function(i, point) {
                     s += '<tr><td><span style="color:' + point.series.color + '">' + point.series.name + '</span></td>';
-                    s += '<td align="right">' + Highcharts.dateFormat('%H h %M min %S s', point.y) + '</td></tr>';
+                    
+                    if (jQuery.inArray(point.series.name, duration) > -1) {
+                        s += '<td colspan="2" align="right">' + Highcharts.dateFormat('%H h %M min %S s', point.y) + '</td>';
+                    } else {
+                        s += '<td align="right">' + Highcharts.numberFormat(point.y, 0, '', ' ') + '</td>';
+                        s += '<td align="right">' + Highcharts.numberFormat(point.percentage, 0) + ' %</td>';
+                    }
+                    
+                    s+= "</tr>";
                 });
                             
                 s += '</table>';
@@ -359,23 +393,40 @@ $(function () {
             shared: true,
             useHTML: true
         },
+        xAxis: {
+            dateTimeLabelFormats: {
+                day: '%e %b',
+                week: '%e %b',
+                month: '%b %y'
+            },
+            gridLineDashStyle: 'dot',
+            gridLineWidth: 1,
+            minTickInterval: 24 * 3600 * 1000 // 1 day
+        },
         yAxis: [
             {
                 type: 'datetime',
+                height: 250,
                 lineWidth: 1,
                 gridLineDashStyle: 'dot',
                 title: {
-                    text: null
+                    text: 'Duration (hours and minutes)'
                 },
                 min: 0,
-                showFirstLabel: false,
-                maxZoom: 0.1
+                showFirstLabel: false
             }, {
-                opposite: true,
-                linkedTo: 0,
-                type: 'datetime',
+                height: 250,
+                top: 350,
                 lineWidth: 1,
-                gridLineWidth: 0,
+                labels: {
+                    formatter: function() {
+                        return this.value + ' %'
+                    }
+                },
+                offset: 0,
+                title: {
+                    text: 'Status'
+                },
                 showFirstLabel: false
             }
         ]
